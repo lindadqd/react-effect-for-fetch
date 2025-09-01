0d98511de64fb144224d70219ac47c453cef197a
Linda Do <lindado_98@hotmail.com>
2025-09-01 13:32:07 +0200

    core

--- src/sections/Art/index.jsx
+++ src/sections/Art/index.jsx
@@ -1,8 +1,45 @@
+import{ useEffect, useState } from 'react'
+
 function ArtsSection() {
+  const baseUrl = "https://boolean-uk-api-server.fly.dev"
+  const url = "https://boolean-uk-api-server.fly.dev/art"
+  const [filteredData, setFilteredData] = useState([])
+
+  useEffect(() => {
+    const fetchData = async () => {
+    const response = await fetch(url)
+    const jsonData = await response.json()
+    setFilteredData(jsonData)
+  }
+  fetchData()
+  }, [])
+
+
+
   return (
     <section>
       <h2>Arts Section</h2>
-      <div className="scroll-container"></div>
+      <div className="scroll-container">
+        <ul className='art-list'>
+          <li>
+            {filteredData.map((art, index) => (
+              <div className= "frame" key={index}>
+              <img src={baseUrl + art.imageURL} alt={art.name} key={index}/>
+              {console.log(baseUrl + art.image)}
+              <h3>{art.title}</h3>
+              <p>{"Artist: " + art.artist}</p>
+              <h4>Publication History: </h4>
+              <ul>
+                <li>{art.publicationHistory[0]}</li>
+                <li>{art.publicationHistory[1]}</li>
+                <li>{art.publicationHistory[2]}</li>
+              </ul>
+              </div>
+            ))}
+            
+          </li>
+        </ul>
+      </div>
     </section>
   )
 }
--- src/sections/Users/index.jsx
+++ src/sections/Users/index.jsx
@@ -1,8 +1,33 @@
+import{ useEffect, useState } from 'react'
+
 function UsersSection() {
+  const url = "https://boolean-uk-api-server.fly.dev/lindadqd/contact"
+
+  const [filteredData, setFilteredData] = useState([])
+
+  useEffect(() => {
+    const fetchData = async () => {
+    const response = await fetch(url)
+    const jsonData = await response.json()
+    setFilteredData(jsonData)
+  }
+  fetchData()
+  }, [])
+
   return (
     <section>
       <h2>Users Section</h2>
-      <div className="scroll-container"></div>
+      <div className="scroll-container">
+        <ul className= "users-list">
+          {filteredData.map((user, index) => (
+              <li key={index} style={{ background: user.favouriteColour }}>
+              <img src={user.profileImage}/>
+              <h3>{user.firstName +" "+ user.lastName}</h3>
+              <p>Email: {user.email}</p>
+              </li>
+            ))}
+        </ul>
+      </div>
     </section>
   )
 }
